dd2d4994e7225d9ee38a4332ff8c42c63d50098a
haimer.barbetti <ec0326z@comcel.com.co>
2023-08-01 21:45:23 -0500

    inicia la implementación del frontend reac

--- api/api.php
+++ api/api.php
@@ -1,12 +1,48 @@
 <?php
 
+
+header("Access-Control-Allow-Origin: *");
+
 /**
  * Api base
  * Metodos: POST, PUT, GET etc
  * 
  */
 
- 
+
+
+
+//$protocol = stripos($_SERVER['SERVER_PROTOCOL'],'https') === true ? 'https://' : 'http://';
+
+//$domain = $_SERVER['HTTP_HOST'];
+
+//echo $domain;
+
+/**
+ * @author 
+ * 
+ * 
+ */
+
+$metodo=$_SERVER['REQUEST_METHOD'];
+
+if($metodo=='GET'){
+    //implementar el metodo GET aqui
+    echo "Metodo GET ";
+}
+
+if($metodo=='POST'){
+    //implementar el metodo POST aqui
+    echo "Metodo POST ";
+}
+
+if($metodo=='PUT'){
+    //implementar el metodo PUT aqui
+}
+
+if($metodo=='DELETE'){
+    //implementar el metodo DELETE aqui
+}
 
 
  ?>
